c0a76dcef2e7cf8a0392f97175ece39f58903ccb
augusto <augustoluisperes@gmail.com>
2023-02-13 23:43:38 +0000

    finishing training setup

--- vqvae/lightning_modules.py
+++ vqvae/lightning_modules.py
@@ -12,13 +12,21 @@ class VQVAEWrapper(pl.LightningModule):
         self.learning_rate = learning_rate
 
     def training_step(self, batch, _):
-        loss, reconstruction_loss = self.compute_loss(batch)
+        loss, reconstruction_loss, images, reconstructions = self.compute_loss(
+            batch)
         self.log('codebook_loss', loss, prog_bar=True)
         self.log('reconstruction_loss', reconstruction_loss, prog_bar=True)
-        return loss + reconstruction_loss
+        return {
+            'loss': loss + reconstruction_loss,
+            'codebook_loss': loss,
+            'reconstruction_loss': reconstruction_loss,
+            'images': images,
+            'reconstructions': reconstructions
+        }
 
     def validation_step(self, batch, _):
-        loss, reconstruction_loss = self.compute_loss(batch)
+        loss, reconstruction_loss, images, reconstructions = self.compute_loss(
+            batch)
         self.log('codebook_loss_val', loss, on_epoch=True, prog_bar=True)
         self.log('reconstruction_loss_val',
                  reconstruction_loss,
@@ -26,14 +34,17 @@ class VQVAEWrapper(pl.LightningModule):
                  prog_bar=True)
         return {
             'codebook_loss_val': loss,
-            'reconstruction_loss_val': reconstruction_loss
+            'reconstruction_loss_val': reconstruction_loss,
+            'images': images,
+            'reconstructions': reconstructions
         }
 
     def compute_loss(self, batch):
         images = batch['image']
         reconstructions, _, _, loss = self.model(images)
         reconstruction_loss = torch.nn.MSELoss()(reconstructions, images)
-        return loss, reconstruction_loss
+        return loss, reconstruction_loss, images.detach(
+        ), reconstructions.detach()
 
     def configure_optimizers(self):
         """Wrap the optimizer into a pl optimizer. """
